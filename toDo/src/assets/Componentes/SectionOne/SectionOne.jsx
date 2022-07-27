aba100a9b15f29c2358f33aceed6217af61755ca
felipehdev <felipehdev@gmail.com>
2022-07-27 11:20:27 -0300

    modificaçoes nav e section

---
+++ toDo/src/assets/Componentes/SectionOne/SectionOne.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import S from './SectionOne.module.css'
+
+const SectionOne = () => {
+  return (
+    <div className={S.section}></div>
+  )
+}
+
+export default SectionOne
