1205ffd1b639e2e9982be9bace7bb71467cc4a22
felipehdev <felipehdev@gmail.com>
2022-07-27 10:52:09 -0300

    Navbar e Section

--- toDo/src/App.jsx
+++ toDo/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './assets/Componentes/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
