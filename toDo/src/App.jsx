a0fa6343cad45a8cad46b54c21c7dbfd3ff0af4e
Eduardo Victor <100290358+eduu25@users.noreply.github.com>
2022-07-27 10:55:48 -0300

    Buscando usuarios

--- toDo/src/App.jsx
+++ toDo/src/App.jsx
@@ -6,7 +6,7 @@ const [requisicao, setRequisicao] = useState()
 
 function App() {
   async function req(){
-   const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+   const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
    const json = await response.json()
    setRequisicao(json)
   }
